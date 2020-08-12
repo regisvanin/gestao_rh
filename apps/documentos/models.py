f7bcd09a5187d493fbf55a500277831188a16f3d
Reginaldo Vanin <regisvanin@gmail.com>
2020-08-12 20:03:35 -0300

    alteracao do pycache

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from apps.funcionarios.models import Funcionario
 
 class Documento(models.Model):
     descricao = models.CharField(max_length=100)
+    pertence = models.ForeignKey(Funcionario, on_delete=models.PROTECT) # seta nulo quando for deletado o funcionario / null and black
 
     def __str__(self):
         return self.descricao
--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from apps.departamentos.models import Departamento
+from apps.empresas.models import Empresa
 
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.PROTECT) # validacao com django user
+    departamentos = models.ManyToManyField(Departamento) # um funcionario podera ter uma lista de departamentos
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.nome
