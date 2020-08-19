8209473d2a5f96725fc17c095b5c7cd1106e45b7
Reginaldo Vanin <regisvanin@gmail.com>
2020-08-19 20:33:56 -0300

    aula 31

--- apps/departamentos/views.py
+++ apps/departamentos/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .models import Departamento
 
-# Create your views here.
+
+class DepartamentosList(ListView):
+    model = Departamento
+
+    def get_queryset(self):
+        empresa_logada = self.request.user.funcionario.empresa
+        queryset = Departamento.objects.filter(empresa=empresa_logada)
+        return queryset
+
+class DepartamentoCreate(CreateView):
+    model = Departamento
+    fields = ['nome']
+
+    def form_valid(self, form):
+        departamento = form.save(commit=False)  # cria o objeto em memoria e nao manda para o banco
+        departamento.empresa = self.request.user.funcionario.empresa
+        departamento.save()
+        return super(DepartamentoCreate, self).form_valid(form)
+
+class DepartamentoUpdate(UpdateView):
+    model = Departamento
+    fields = ['nome']
+
+class DepartamentoDelete(DeleteView):
+    model = Departamento
+    success_url = reverse_lazy('list_departamentos')
--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
+
 from apps.departamentos.models import Departamento
 from apps.empresas.models import Empresa
 
@@ -7,7 +9,12 @@ class Funcionario(models.Model):
     nome = models.CharField(max_length=100)
     user = models.OneToOneField(User, on_delete=models.PROTECT) # validacao com django user
     departamentos = models.ManyToManyField(Departamento) # um funcionario podera ter uma lista de departamentos
-    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT, null=True, blank=True)
+
+    def get_absolute_url(self):
+        return reverse('list_funcionarios')
+
+
 
     def __str__(self):
         return self.nome
