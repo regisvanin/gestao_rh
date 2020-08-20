725e4b731c81c0a9d791dcd946c53c432bec4327
Reginaldo Vanin <regisvanin@gmail.com>
2020-08-20 19:45:26 -0300

    Aula 37

--- apps/registro_hora_extra/views.py
+++ apps/registro_hora_extra/views.py
@@ -1,5 +1,7 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, UpdateView, DeleteView, CreateView
+
+from .forms import RegistroHoraExtraForm
 from .models import RegistroHoraExtra
 
 
@@ -14,7 +16,14 @@ class HoraExtraList(ListView):
 # UPDATE possui o objeto object dentro dele..
 class HoraExtraEdit(UpdateView):
     model = RegistroHoraExtra
-    fields = ['motivo', 'funcionario', 'horas']
+    #fields = ['motivo', 'funcionario', 'horas']
+    form_class = RegistroHoraExtraForm
+
+    def get_form_kwargs(self):
+        kwargs = super(HoraExtraEdit, self).get_form_kwargs()
+        kwargs.update({'user': self.request.user})
+        return  kwargs
+
 
 class HoraExtraDelete(DeleteView):
     model = RegistroHoraExtra
@@ -22,4 +31,11 @@ class HoraExtraDelete(DeleteView):
 
 class HoraExtraCreate(CreateView):
     model = RegistroHoraExtra
-    fields = ['motivo', 'funcionario', 'horas']
+    form_class = RegistroHoraExtraForm
+    # fields = ['motivo', 'funcionario', 'horas']
+
+    # metodo utilizado para pegar usuario logado e injetar no form
+    def get_form_kwargs(self):
+        kwargs = super(HoraExtraCreate, self).get_form_kwargs()
+        kwargs.update({'user': self.request.user})
+        return  kwargs
