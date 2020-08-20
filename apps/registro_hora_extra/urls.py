019b2860094eac8c4301c4480e50d7d3f20e43a7
Reginaldo Vanin <regisvanin@gmail.com>
2020-08-20 19:19:43 -0300

    Aula 36

--- apps/registro_hora_extra/urls.py
+++ apps/registro_hora_extra/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import HoraExtraList  #FuncionarioEdit, FuncionarioDelete, FuncionarioNovo
+from .views import (HoraExtraList,
+                    HoraExtraEdit,
+                    HoraExtraDelete,
+                    HoraExtraCreate,
+                    )
+
 
 urlpatterns = [
     path('', HoraExtraList.as_view(), name='list_hora_extra'),
-    #path('editar/<int:pk>/', FuncionarioEdit.as_view(), name='update_funcionario'),
-    #path('delete/<int:pk>/', FuncionarioDelete.as_view(), name='delete_funcionario'),
-    #path('novo', FuncionarioNovo.as_view(), name='create_funcionario'),
+    path('editar/<int:pk>/', HoraExtraEdit.as_view(), name='update_hora_extra'),
+    path('delete/<int:pk>/', HoraExtraDelete.as_view(), name='delete_hora_extra'),
+    path('novo', HoraExtraCreate.as_view(), name='create_hora_extra'),
 ]
--- apps/registro_hora_extra/views.py
+++ apps/registro_hora_extra/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, UpdateView, DeleteView, CreateView
 from .models import RegistroHoraExtra
 
 
@@ -10,3 +11,15 @@ class HoraExtraList(ListView):
         queryset = RegistroHoraExtra.objects.filter(funcionario__empresa=empresa_logada) # olhando somente empresa logada e do funcionario
         return queryset
 
+# UPDATE possui o objeto object dentro dele..
+class HoraExtraEdit(UpdateView):
+    model = RegistroHoraExtra
+    fields = ['motivo', 'funcionario', 'horas']
+
+class HoraExtraDelete(DeleteView):
+    model = RegistroHoraExtra
+    success_url = reverse_lazy('list_hora_extra')
+
+class HoraExtraCreate(CreateView):
+    model = RegistroHoraExtra
+    fields = ['motivo', 'funcionario', 'horas']
