055d9e555d6a7100d8ec83f272edffff238c81a4
Reginaldo Vanin <regisvanin@gmail.com>
2020-09-28 19:20:53 -0300

    Espondo API Django Rest Framework - Aula 53

--- apps/core/views.py
+++ apps/core/views.py
@@ -8,3 +8,28 @@ def home(request):
     data = {}
     data['usuario'] = request.user
     return render(request, 'core/index.html', data)
+
+
+
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from apps.core.serializers import UserSerializer, GroupSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer # template para montar os dados
+    permission_classes = [permissions.IsAuthenticated]
